03c71bdf7214fd533cec4341efac1d1acc7f32e2
flare-s <webflarez@gmail.com>
2022-11-08 16:09:06 +0000

    Co-authored-by: yassienAbdillahi <yassienAbdillahi@users.noreply.github.com>

---
+++ pages/users/[id].js
@@ -0,0 +1,37 @@
+import ProtectedRoute from "../../components/ProtectedRoute";
+//import getUser function from firebase/firestore.js
+export async function getServerSideProps({ params }) {
+  //params.id is the user's id
+
+  //Delete the following 7 lines
+  // console.log(params);
+  // const db = getFirestore(app);
+  // const quotesCol = collection(db, "quotes");
+  // const snapShot1 = await getDocs(quotesCol);
+  // snapShot1.forEach((snap) => {
+  //   console.log(snap.data());
+  // });
+
+  //get current user's details (their name, quotes etc) from database
+
+  return {
+    props: {
+      userData: {
+        name: "Amy",
+        quotes: [{}, {}] /* this is just dummy data for now */,
+      },
+    },
+  };
+}
+
+const DashboardPage = ({ userData }) => {
+  return (
+    <ProtectedRoute>
+      <div>
+        <h1>{userData.name}</h1>
+      </div>
+    </ProtectedRoute>
+  );
+};
+
+export default DashboardPage;
