c9e48775945a8cfddac87d0ea1317041bc9cca04
y <johndoe@example.com>
2022-11-17 14:28:34 +0000

    Commented out alerts for errors. Now just the console.log of the error remains

--- components/SaveQuoteForm.js
+++ components/SaveQuoteForm.js
@@ -37,7 +37,7 @@ export default function SaveQuoteForm() {
       });
     } catch (error) {
       console.log(error.message);
-      alert(error.message);
+      // alert(error.message);
     }
   };
 
